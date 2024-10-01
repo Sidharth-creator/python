3c12ee629b3d3952fd2c899064d5ac01c37e15a3
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-01 15:19:53 +0530

    Add files via upload
    document header added

--- student details.py
+++ student details.py
@@ -1,7 +1,12 @@
+#author: Sidharth.S
+#date 01-10-2024
+#python programme to get students details
+#version:1.0
+
 name=input("enter the name of the Student:")
 rollno=int(input("enter the roll number of student:"))
 cgpa=float(input("enter the cgp of the student:"))
 rollno=rollno+1
 print("Name of student=",name)
 print("Roll number of the student=",rollno)
-print("CGPA of the student in percentage =",cgpa*10,"%")
+print("CGPA of   student in percentage =",cgpa*10,"%")
