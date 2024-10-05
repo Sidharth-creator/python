e4069f81b84884fc010d6f747b737b888a404a8a
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-06 02:06:37 +0530

    Add files via upload
    math module

---
+++ math_module.py
@@ -0,0 +1,10 @@
+'''
+Author: Sidharth.S
+Date: 05-10-2024
+This program uses specific functions in math module to perform operations
+'''
+num1=int(input("Enter a Number:"))
+import math
+print("Square root of",num1,":",math.sqrt(num1))
+print("Factorial of",num1,":",math.factorial(num1))
+print(num1,"raised to power 2:",math.pow(5,2))
