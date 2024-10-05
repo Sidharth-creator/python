77454488b86b2be277123eb8cbff5ca93e5a8439
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-06 04:43:49 +0530

    Add files via upload
    string extraction and concatnation

---
+++ string extraction and concatnation.py
@@ -0,0 +1,10 @@
+'''
+Author: Sourav V S
+Date:05-10-2024
+This program is to extract a substring from a string and concatenate it with another string
+'''
+str1="Hello, World!"
+str2=str1[7:12]
+str3=" Everyone!"
+newstr= str2+str3
+print(newstr)
