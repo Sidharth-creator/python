1234eca4c48319996fed4ef4ac43607a702ec810
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-07 22:23:57 +0530

    Update operators_in_python.py

---
+++ operators_in_python.py
@@ -0,0 +1,17 @@
+'''
+Author: Sidharth.S
+Date: 05-10-2024
+This program is to demonstrate various operations in python
+'''
+a=10
+b=5
+sum=a+b
+div=a/b
+c1=a>b
+c2=b<a
+
+print("Sum:",sum,", Division:",div)
+print("Is a greater than b?:",c1)
+print("Are a and b equal?:",a==b)
+print("Logical AND:",c1)
+print("Logical OR:",c2)
