bee65a527da9428903b8c5e9c67c43202b9b7e6e
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-07 20:32:06 +0530

    Update name and age.py
    name and age of studentd

---
+++ name and age.py
@@ -0,0 +1,8 @@
+'''
+Author: Sidharth.S
+Date: 05-10-2024
+This program is to get the name and age of person
+'''
+name= input("Enter Your Name:")
+age= input("Enter Your Age:")
+print("Hello,",name,"! You are",age,"years old")
