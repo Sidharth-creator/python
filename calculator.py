781b681aed9df55f00b7a858deab3f74d1a26d0f
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-08 14:19:21 +0530

    Add files via upload
    calculator

---
+++ calculator.py
@@ -0,0 +1,20 @@
+number1=int(input("enter the first number:"))
+number2=int(input("entre the second number"))
+number3=int(input("enter te third number:"))
+sum=number1+number2
+sub=number1-number2
+product=number1*number2
+div=number1/number2
+mod=number1%number2
+arith=(number1+number2)*number3/2
+print("sum of the numbers is:",sum)
+print("difference of the number is:",sub)
+print("product of the number is:",product)
+print("division of the numbers is:",div)
+print("the reminders of given numbers:",mod)
+print("the result of (number1+number2)*number3/2 :",arith)
+
+
+
+
+
