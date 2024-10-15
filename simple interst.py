285af4e9066df15ae515feb40420fb58542bf14d
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:17:56 +0530

    Add files via upload
    code to find the simple interest

---
+++ simple interst.py
@@ -0,0 +1,5 @@
+principle=int(input("enter the principl number:"))
+no_of_year=int(input("enter the no of year:"))
+rate=float(input("enter the rate:"))
+simple_interest=principle*no_of_year*rate
+print(simple_interest)
