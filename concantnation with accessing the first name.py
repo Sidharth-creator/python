7cd1711e05f9399c8a0d044c0ff4bb79b03dbd46
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:56:19 +0530

    Update concantnation with accessing the first name.py

--- concantnation with accessing the first name.py
+++ concantnation with accessing the first name.py
@@ -1,3 +1,11 @@
+
+'''author:Sidharth.S
+  Date:15-10-2024
+  this program is to find the positive number and negative number'''
+
+
+
+
 firstname=input("enter the first  name:")
 lastname=input("enter your last name:")
 fullname=firstname+" "+lastname
