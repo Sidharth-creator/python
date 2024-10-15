ecee4aef04c1e5cedd3bab402892bbf22e5f589d
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:10:58 +0530

    Add files via upload
    to find if the given number is positive or negative

---
+++ positive  or negative.py
@@ -0,0 +1,12 @@
+'''author:Sidharth.S
+  Date:15-10-2024
+  this program is to find the positive number and negative number'''
+
+
+number=int(input("enter the first number:"))
+if number > 0:
+    print(number,"is positive")
+elif number < 0:
+    print(number,"is negative")
+else:
+    print("the given number is o")
