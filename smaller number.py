735bd812290cd28b1c08fe7467456656e2264307
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:33:06 +0530

    Add files via upload
    to find the smallest number from three numbers

---
+++ smaller number.py
@@ -0,0 +1,11 @@
+number1=int(input("enter the first nuber:"))
+number2=int(input("enter the second number:"))
+number3=int(input("enter ten third number:"))
+if number1<number2 and number1<number3:
+    print(number1,"is the smallest")
+elif number2<number1 and number2<number3:
+    print(number2,"is the smallest")
+elif number3<number1 and number3<number2:
+    print(number3,"is the smallest")
+else:
+    print("they are equal")
