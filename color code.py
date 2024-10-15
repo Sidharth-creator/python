631e23003450fb4e2ad4717dbcb830faf4b134c7
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:48:52 +0530

    Add files via upload
    to find the colour by using the colour code

---
+++ color code.py
@@ -0,0 +1,9 @@
+colour=input("enter the colour code:")
+if colour=="R" or colour=="r":
+    print("the color is RED")
+elif colour=="G" or colour=="g":
+    print("the colour is GREEN")
+elif colour=="B" or colour=="b":
+    print("the colour is BLUE")
+else:
+    print("WRONG COLOUR CODE!")
