414d908dbb881e15f5b75e6484482cfdb7791910
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:58:19 +0530

    Update simple interst.py

--- simple interst.py
+++ simple interst.py
@@ -1,3 +1,10 @@
+
+'''author:Sidharth.S
+  Date:15-10-2024
+  this program is to find the positive number and negative number'''
+
+
+
 principle=int(input("enter the principl number:"))
 no_of_year=int(input("enter the no of year:"))
 rate=float(input("enter the rate:"))
