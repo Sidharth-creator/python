ac7b8881c6afdc3ed3c2e7d96aec3daf459d18b8
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-11-19 16:08:26 +0530

    Add files via upload
    printing stars in ordered way

---
+++ pattern3.py
@@ -0,0 +1,17 @@
+lim=int(input("enter the no:"))
+for j in range(1,lim+1):
+    for i in range(lim-j):
+        print(" ",end="")
+    for k in range(2*j-1):
+        print("*",end="")
+    print()
+
+
+
+
+
+
+
+
+
+
